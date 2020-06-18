cccc99bfb6d207ff234304b4db74eeea33168339
VitalyMikulich <mr.wital1999@gmail.com>
2020-06-18 22:25:35 +0300

    upload on gh-pages

---
+++ precache-manifest.2bd5ea9b262748cd882be87779360701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74e8415594ed9654464a8cfdaf601d3",
+    "url": "/APOD-test-task/index.html"
+  },
+  {
+    "revision": "eeb15737b749505bf5b7",
+    "url": "/APOD-test-task/static/css/main.1a8cee7b.chunk.css"
+  },
+  {
+    "revision": "5c5275da38f834441092",
+    "url": "/APOD-test-task/static/js/2.8a5a4bf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/APOD-test-task/static/js/2.8a5a4bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb15737b749505bf5b7",
+    "url": "/APOD-test-task/static/js/main.e2fa4bcb.chunk.js"
+  },
+  {
+    "revision": "6bf096ff194ac2194058",
+    "url": "/APOD-test-task/static/js/runtime-main.c2ec5968.js"
+  }
+]);
